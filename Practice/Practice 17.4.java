15c44a420b0c2091b1ac99cf6f9cf71c654fa6f6
sidestep9 <renhirtana@yahoo.com>
2025-05-22 11:46:23 +0700

    Add files via upload

---
+++ Practice/Practice 17.4.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int height;
+		int width;
+		char symbol;
+		
+		height = 5;
+		width = 5;
+		symbol = '*';
+		
+		for(int i = 1; i <= height; i++) {
+			for(int j = 1; j <= height - i; j++) {
+				System.out.print("x");
+			}
+			for(int k = 1; k <= i; k++){
+				System.out.print(symbol);
+			}
+			System.out.println();
+		}
+		
+			
+		scanner.close();	
+		
+	}
+}
