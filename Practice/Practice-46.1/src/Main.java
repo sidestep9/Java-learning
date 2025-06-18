6c4ba98dc4bf0303ce88d61c39f9f9d0405427dd
sidestep9 <renhirtana@yahoo.com>
2025-06-18 18:34:09 +0900

    parctie)

--- Practice/Practice-46.1/src/Main.java
+++ Practice/Practice-46.1/src/Main.java
@@ -7,18 +7,49 @@ public class Main {
     public static void main(String[] args) {
         int numOfTask;
         ArrayList<String> taskName = new ArrayList<>();
-        ArrayList<Double> duration = new ArrayList<>();
+        ArrayList<Integer> duration = new ArrayList<>();
+        int hours;
+        int minutes;
+        int total = 0;
+        String longest = "";
+        int max = 0;
         
         numOfTask = InputHandler.inputInt("How many tasks: ");
         System.out.println();
         
         while(numOfTask > 0) {
             taskName.add(InputHandler.inputText("Enter task name: "));
-            duration.add(InputHandler.inputDouble("How long (minutes): "));
+            duration.add(InputHandler.inputInt("How long (minutes): "));
             System.out.println();
             
             numOfTask--;
         }
+        
+        System.out.println("=== Daily Task Summary ===\n");
+        
+        for(int i = 0; i < taskName.size(); i++) {
+            hours = duration.get(i) / 60;
+            minutes = duration.get(i) % 60;
+            total += duration.get(i);
+            
+            if(i == 0) {
+                longest = taskName.get(i);
+                max = duration.get(i);
+            }
+            if(i != 0 && duration.get(i) == max) {
+                longest += ", " + taskName.get(i);
+            }
+            if(i != 0 && duration.get(i) > max) {
+                longest = taskName.get(i);
+                max = duration.get(i);
+            }
+            
+            
+            System.out.printf("%d. %s - %d hours %d minutes\n", i + 1, taskName.get(i), hours, minutes);
+        }
+        
+        System.out.printf("\nTotal time: %d hours %d minutes\n", total / 60, total % 60);
+        System.out.println("Longest task(s): " + longest);
     }
     
 }
