39a9a5954c2acec45cfd6018fdaae0ed1cfb71a4
sidestep9 <renhirtana@yahoo.com>
2025-08-12 14:22:23 +0900

    leetcode and tezt ssh

--- Leetcode-dummy.java
+++ Leetcode-dummy.java
@@ -1,38 +1,7 @@
+
 class Solution {
-    public boolean isValid(String s) {
+    
+    public int mySqrt(int x) {
         
-        if(s.length() == 0 || s.length() == 1) return false;
-        
-        char[] stack = new char
-        [s.length()];
-
-        int top = 0;
-
-        for(int i = 0; i < s.length(); i++) {
-            char o = s.charAt(i);
-            
-            if(i == s.length() - 1 && (o == '(' || o == '{' || o == '[')) {
-                return false;
-            }
-
-            if(o == '(' || o == '{' || o == '[') {
-                stack[top++] = o;
-            }
-            else if(o == ')') {
-                if(stack[--top] != '(') {
-                    return false;
-                }
-            }
-            else if(o == '}') {
-                if(stack[--top] != '{') {
-                    return false;
-                }
-            }
-            else if(o == ']') {
-                if(stack[--top] != '[') {
-                    return false;
-                }
-            }
-        }
     }
 }
